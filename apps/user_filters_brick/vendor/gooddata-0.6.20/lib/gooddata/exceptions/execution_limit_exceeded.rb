a978e883ca9aba168f46378485a5967dfaff29d1
Tomas Svarovsky <svarovsky@gooddata.com>
2016-07-12 13:17:30 -0700

    Deleted old bricks from app_store that were moved to appstore

--- apps/user_filters_brick/vendor/gooddata-0.6.20/lib/gooddata/exceptions/execution_limit_exceeded.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-module GoodData
-  class ExecutionLimitExceeded < RuntimeError
-    def initialize(msg = 'The time limit #{time_limit} secs for polling on #{link} is over')
-      super(msg)
-    end
-  end
-end
--- apps/user_filters_brick/vendor/gooddata-0.6.20/lib/gooddata/exceptions/uncomputable_report.rb
+++
@@ -1,8 +0,0 @@
-# encoding: UTF-8
-require 'restclient/exceptions'
-
-module GoodData
-  # Project Not Found
-  class UncomputableReport < RuntimeError
-  end
-end
